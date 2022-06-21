0f482ea468aa0c4a6007d2d9e2e2e57f5e8cb9ee
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-06-21 15:43:09 -0300

    removed footer from Foods page

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Footer from '../components/Footer';
 
 export default function Foods() {
   return (
     <div>
       <h1>Foods</h1>
-      <Footer />
     </div>
   );
 }
