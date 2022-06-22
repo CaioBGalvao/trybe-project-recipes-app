1219e5066a5050b19305b5b084142fd9289caf90
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-06-21 21:47:58 -0300

    criado a tela de explore

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+// Falta Footer
 
 function Explore() {
   return (
     <div>
       <Header title="Busca" />
+      <Link to="/explore/foods">
+        <button
+          type="button"
+          data-testid="explore-foods"
+        >
+          Explore Foods
+
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+        >
+          Explore Drinks
+
+        </button>
+      </Link>
     </div>
   );
 }
