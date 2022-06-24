d5be575a30acd56fb7f310ec3a95b14806a6d4c9
Pamela <pamcasatte@gmail.com>
2022-06-24 14:36:43 -0300

    página explore-foods-drinks-e-profile

--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,9 +7,32 @@ function ExploreFoods() {
   return (
     <div>
       <Header title="Explore Foods" btnSearch />
+      <Link to="/explore/foods">
+        <button type="button" data-testid="explore-by-ingredient">By Ingredient</button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+      </Link>
+      <Link to="/foods">
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise Me!
+
+        </button>
+      </Link>
       <Footer />
     </div>
   );
 }
 
 export default ExploreFoods;
+
+// falta fazer chamanda da API para buscar receita;
+// - Ao clicar no botão "Surprise me!" da tela de explorar comidas a rota muda para a página de detalhes de uma comida aleatória
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,9 +7,40 @@ function Profile() {
   return (
     <div>
       <Header title="Profile" btnSearch />
+      <h3 data-testid="profile-email">Email do Usuário</h3>
+      <Link to="/done-recipes">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+
+        </button>
+      </Link>
+      <Link to="/favorites-recipes">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </Link>
       <Footer />
     </div>
   );
 }
 
 export default Profile;
+
+// Falta
+// - Limpa todas as chaves da localStorage;
+// - O e-mail armazenado em localStorage está visível.
