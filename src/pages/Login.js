8d385a7066847644d3ed58dc137ef0932853db20
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-01 11:37:47 -0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import LoginContext from '../context/LoginContext';
 
+// a
+
 export default function Login({ history }) {
   const {
     setInputEmail,
